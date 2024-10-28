178834141329ce9955dbf4cd3578995822dec622
Abdul Rehman <abdulrehmanmangral86@gmail.com>
2024-10-28 23:53:30 +0500

    pushing files

---
+++ app/api/api_v1/main.py
@@ -0,0 +1,9 @@
+from fastapi import APIRouter
+
+from app.api.api_v1.endpoints import auth, property, superadmin
+
+api_router = APIRouter()
+
+api_router.include_router(property.router, prefix="/property", tags=["Properties"])
+api_router.include_router(superadmin.router, prefix="/superadmin", tags=["Superadmin"])
+api_router.include_router(auth.router, prefix="/auth", tags=["Auth"])
---
+++ app/models/user.py
@@ -0,0 +1,20 @@
+from sqlalchemy import Boolean, String, Integer, LargeBinary, Enum
+from sqlalchemy.orm import Mapped, mapped_column, relationship
+from app.core.db import Base
+from enum import Enum as PEnum
+
+from app.models.mixin import SharedMixin
+
+
+
+class User(Base,SharedMixin):
+    __tablename__ = "users"
+
+    first_name: Mapped[str] = mapped_column(String(100), nullable=False)
+    last_name: Mapped[str] = mapped_column(String(100), nullable=False)
+    username: Mapped[str] = mapped_column(String(255), unique=True, nullable=False)
+    password: Mapped[LargeBinary] = mapped_column(LargeBinary)
+    password_salt: Mapped[LargeBinary] = mapped_column(LargeBinary)
+    is_super_admin: Mapped[bool] = mapped_column(Boolean)
+    is_active: Mapped[bool] = mapped_column(Boolean, default=True)
+
